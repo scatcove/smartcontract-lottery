0e0371a6771a775d4425461779ffdbeac7d5d034
u7104627 <u7104627@anu.edu.au>
2022-02-12 16:40:53 +1100

    update

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.6.6;
 
 import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
+import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
 
 contract Lottery is VRFConsumerBase, Ownable {
     enum LOTTERY_STATE {
@@ -18,13 +18,14 @@ contract Lottery is VRFConsumerBase, Ownable {
     AggregatorV3Interface internal ethUsdPriceFeed;
     uint256 public fee;
     bytes32 public keyhash;
-
+    address payable public recentWinner;
+    uint256 public randomness;
 
     constructor(
         address _priceFeedAddress,
         address _vrfCoordinator,
         address _link,
-        uint256 _fee.
+        uint256 _fee,
         bytes32 _keyhash
     ) public VRFConsumerBase(_vrfCoordinator, _link) {
         usdEntryFee = 50 * 10**18;
@@ -68,5 +69,23 @@ contract Lottery is VRFConsumerBase, Ownable {
         ) % players.length; */
         lottery_state = LOTTERY_STATE.CALCULATING_WINNER;
         bytes32 requestId = requestRandomness(keyhash, fee);
+    }
+
+    function fulfillRandomness(bytes32 _requestid, uint256 _randomness)
+        internal
+        override
+    {
+        require(
+            lottery_state == LOTTERY_STATE.CALCULATING_WINNER,
+            "You aren't there yet"
+        );
+        require(_randomness > 0, "random-not-found");
+        uint256 indexOfWinner = _randomness % players.length;
+        recentWinner = players[indexOfWinner];
+        recentWinner.transfer(address(this).balance);
+        //Reset Lottery
+        players = new address payable[](0);
+        lottery_state = LOTTERY_STATE.CLOSED;
+        randomness = _randomness;
     }
 }
